623ba585ac312f24baab1f00363f35f891333bef
Yvette <yvette.wouters@rmit.edu.au>
2017-04-20 15:07:31 +1000

    Mockup of profile screen

---
+++ Assets/Scripts/_SceneScripts/Profile/TernaryPlot.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TernaryPlot : MonoBehaviour {
+
+	// Values for equation
+	public float a;
+	public float b;
+	public float c;
+
+	// Point value
+	public float zX;
+	public float zY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DisplayGraph()
+	{
+		GetValues();
+
+		//Then calculate point with these values
+		CalculatePoint();
+
+		//Finally, draw
+		DrawPoint();
+	}
+
+	public void GetValues ()
+	{
+		// Query server by providing date for LOTD
+
+		// Receive a, b, c
+
+	}
+
+	public void CalculatePoint()
+	{
+		// x value
+		zX = ((1 / 2) * (((2 * b) + c)/(a + b + c)));
+
+		// y value
+		zY = (( Mathf.Sqrt(3) / 2) * (c / (a + b + c)));
+	}
+
+	public void DrawPoint()
+	{
+		// Draw point on graph
+
+	}
+}
---
+++ Assets/Scripts/_SceneScripts/Profile/UserDetails.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserDetails : MonoBehaviour {
+
+	public string setUsername;
+	public string tempUsername;
+	public string setEmail;
+	public string tempEmail;
+	public string setState;
+	public string tempState;
+	public string setCountry;
+	public string tempCountry;
+
+	public bool updateUsername;
+	public bool updateEmail;
+	public bool updateState;
+	public bool updateCountry;
+
+	// Use this for initialization
+	void Start () {
+		GetDetails();
+		ResetFlags();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DisplayDetails()
+	{
+		
+	}
+
+	public void UpdateEmail()
+	{
+		
+	}
+
+	public void UpdateLocation()
+	{
+		
+	}
+
+	public void ConfirmUpdates()
+	{
+		/*
+		//Check if any changes have been made
+		if ()
+		{
+			// Send updated information to server
+		}
+		*/
+
+		UpdateDetails();
+		ResetFlags();
+	}
+
+	public void GetDetails()
+	{
+		// Update fields upon scene load
+
+		// Get username
+		// Get email
+		// Get state
+		// Get country
+
+		// Get trophies
+		DisplayTrophies();
+	}
+
+	public void UpdateDetails()
+	{
+		if (updateUsername){setUsername = tempUsername;}
+		if (updateEmail){setEmail = tempEmail;}
+		if (updateState){setState = tempState;}
+		if (updateCountry){setCountry = tempCountry;}
+	}
+
+	public void ResetFlags()
+	{
+		updateUsername = false;
+		updateEmail = false;
+		updateState = false;
+		updateCountry = false;
+	}
+
+	public void DisplayTrophies()
+	{
+		
+	}
+}
