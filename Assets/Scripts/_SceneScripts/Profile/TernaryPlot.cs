b6bceb6d1f3d380528dc636dc6b57b1a80d14dc2
Yvette <yvette.wouters@rmit.edu.au>
2017-04-24 12:25:40 +1000

    Beginning writing algorithms for calculating points on graphs, interaction for graphs

--- Assets/Scripts/_SceneScripts/Profile/TernaryPlot.cs
+++ Assets/Scripts/_SceneScripts/Profile/TernaryPlot.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class TernaryPlot : MonoBehaviour {
 
-	// Values for equation
-	public float a;
-	public float b;
-	public float c;
+
+	public int points = 100;
+	public int counter = 0;
 
 	// Point value
 	public float zX;
 	public float zY;
 
+	public GameObject graph;
+	public GameObject pointPrefab;
+
+	public List<Weightings> weightingsList = new List<Weightings>();
+
+	public struct Weightings
+	{
+		public float xVal;
+		public float yVal;
+		public float zVal;
+
+		public Weightings(float x, float y, float z)
+		{
+			xVal = x;
+			yVal = y;
+			zVal = z;
+		}
+	}
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		//Dummy values
+		weightingsList.Add(new Weightings(0.1f, 0.1f, 0.8f));
+		weightingsList.Add(new Weightings(0.1f, 0.2f, 0.7f));
+		weightingsList.Add(new Weightings(0.1f, 0.3f, 0.9f));
+		weightingsList.Add(new Weightings(0.3f, 0.2f, 0.5f));
+		weightingsList.Add(new Weightings(0.4f, 0.1f, 0.5f));
+		weightingsList.Add(new Weightings(0.5f, 0.3f, 0.2f));
+		weightingsList.Add(new Weightings(0.2f, 0.7f, 0.1f));
+		weightingsList.Add(new Weightings(0.1f, 0.8f, 0.1f));
 	}
 	
 	// Update is called once per frame
@@ -27,11 +54,23 @@ public class TernaryPlot : MonoBehaviour {
 	{
 		GetValues();
 
-		//Then calculate point with these values
-		CalculatePoint();
+		// Get number of values in list
+		points = weightingsList.Count;
 
-		//Finally, draw
-		DrawPoint();
+		while(counter < points)
+		{
+			//Then calculate point with these values
+			CalculatePoint(	weightingsList[counter].xVal, 
+							weightingsList[counter].yVal,
+							weightingsList[counter].zVal);
+
+			//Finally, draw
+			DrawPoint();
+
+			counter ++;
+		}
+
+		counter = 0;
 	}
 
 	public void GetValues ()
@@ -40,20 +79,34 @@ public class TernaryPlot : MonoBehaviour {
 
 		// Receive a, b, c
 
+		// Add these to list
+
 	}
 
-	public void CalculatePoint()
+	public void CalculatePoint(float a, float b, float c)
 	{
 		// x value
 		zX = ((1 / 2) * (((2 * b) + c)/(a + b + c)));
-
 		// y value
 		zY = (( Mathf.Sqrt(3) / 2) * (c / (a + b + c)));
 	}
 
 	public void DrawPoint()
 	{
-		// Draw point on graph
 
+		//Instantiate
+		GameObject newPoint = Instantiate(pointPrefab);
+
+		//Point name will look like "point(x,y)"
+		string pointName = "point(" + zX.ToString() + "," + zY.ToString() + ")";
+		newPoint.name = pointName;
+
+		//Set parent
+		newPoint.transform.SetParent(graph.transform, false);
+
+		//Set position within parent confines
+		newPoint.transform.localPosition = new Vector2(zX, zY);
+
+		//Add this point GameObject to list
 	}
 }
