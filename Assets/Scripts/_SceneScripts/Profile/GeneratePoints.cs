ce1dba37bfb47a4cf08850a90e67ee2fface10e3
Yvette <yvette.wouters@rmit.edu.au>
2017-04-27 16:16:18 +1000

    3rd graph base code, more placeholders

---
+++ Assets/Scripts/_SceneScripts/Profile/GeneratePoints.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePoints : MonoBehaviour {
+
+	public int points = 100;
+	public int counter = 0;
+
+	public GameObject pointPrefab; 
+
+	//confines of graph object
+	public GameObject graph;
+
+
+
+	Mesh graphMesh;
+
+	// Use this for initialization
+	void Start ()
+	{
+		RequestData();
+		SetGraphConfines();
+		CreatePointList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RequestData()
+	{
+		//Request data from server
+
+		//Add points to list
+
+	}
+
+	public void SetGraphConfines()
+	{
+		//Set dimensions of graph relative to values of points received
+
+		//Get the mesh of the graph
+		graphMesh = graph.GetComponent<MeshFilter>().mesh;
+	}
+
+	public void CreatePointList()
+	{
+		//Reset point lists
+		ResetLists();
+
+		//While there are still points to add
+		while (counter < points)
+		{
+			InstantiatePoint(Random.Range(0f,100f), Random.Range(0f,100f), Random.Range(0f,100f));
+			counter++;
+		}
+	}
+
+	public void InstantiatePoint(float x, float y, float z)
+	{
+		//Instantiate
+		GameObject newPoint = Instantiate(pointPrefab);
+
+		//Point name will look like "point(x,y,z)"
+		string pointName = "point(" + x.ToString() + "," + y.ToString() + "," + z.ToString() + ")";
+		newPoint.name = pointName;
+
+		//Set parent
+		newPoint.transform.SetParent(graph.transform, false);
+
+		//print(graphMesh.bounds.size.x + "," + graphMesh.bounds.size.y + "," +graphMesh.bounds.size.z);
+
+		//Set position within parent confines
+		newPoint.transform.localPosition = new Vector3(	graphMesh.bounds.size.x / x,
+													graphMesh.bounds.size.y / y,
+													graphMesh.bounds.size.z / z);
+
+		//Add this point GameObject to list
+	}
+
+	public void ResetLists()
+	{
+		counter = 0;
+
+		//Reset point list
+		//Reset gameobject list
+
+	}
+}
---
+++ Assets/Scripts/_SceneScripts/Profile/GraphInteraction.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+zoom in = scroll up OR
+zoom out = scroll down OR
+move around selected point = left click + drag
+rotate around centre of screen = right click + drag
+
+NOT YET IMPLEMENTED
+select new point = singular left click
+
+*/
+using UnityEngine.EventSystems;
+
+public class GraphInteraction : MonoBehaviour {
+
+	float zoom = 0.01f;
+
+	float prePosX0;
+	float prePosY0;
+	float prePosX1;
+	float prePosY1;
+
+	float rotX;
+	float rotY;
+	float movX;
+	float movY;
+
+	//RaycastHit hit;
+
+	public GameObject cameraArmRotation;
+	public GameObject cameraArmTranslation;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Using camera for translation / rotations
+		// On left click
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (EventSystem.current.IsPointerOverGameObject())
+			{
+				prePosX0 = Input.mousePosition.x;
+				prePosY0 = Input.mousePosition.y;
+			}
+		}
+
+		// Begin rotating once left dragging commences
+		if (Input.GetMouseButton(0))
+		{
+			if (EventSystem.current.IsPointerOverGameObject())
+			{
+
+				// Get differences between old and new co-ordinates
+				movX = Input.mousePosition.x - prePosX0;
+				movY = Input.mousePosition.y - prePosY0;
+
+				cameraArmTranslation.transform.localPosition += new Vector3((-movX / 100.0f), (-movY / 100.0f), 0f);
+
+				// Set new position to old position
+				prePosX0 = Input.mousePosition.x;
+				prePosY0 = Input.mousePosition.y;
+			}
+		}   
+
+		// On right click
+		if (Input.GetMouseButtonDown(1))
+		{
+			if (EventSystem.current.IsPointerOverGameObject())
+			{
+				prePosX1 = Input.mousePosition.x;
+				prePosY1 = Input.mousePosition.y;
+			}
+		}
+
+		// Begin rotating once right dragging commences
+		if (Input.GetMouseButton(1))
+		{
+			if (EventSystem.current.IsPointerOverGameObject())
+			{
+
+				// Get differences between old and new co-ordinates
+				rotX = Input.mousePosition.x - prePosX1;
+				rotY = Input.mousePosition.y - prePosY1;
+
+				cameraArmRotation.transform.Rotate(new Vector3(-rotY / 2, rotX / 2, 0.0f));
+
+				// Set new position to old position
+				prePosX1 = Input.mousePosition.x;
+				prePosY1 = Input.mousePosition.y;
+			}
+		}  
+
+		//Using camera arm for zooming
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f ) // forward
+		{
+			cameraArmTranslation.transform.localPosition += new Vector3(0f, 0f, zoom);
+		}
+		else if (Input.GetAxis("Mouse ScrollWheel") < 0f ) // backwards
+		{
+			cameraArmTranslation.transform.localPosition -= new Vector3(0f, 0f, zoom);
+		}
+
+	}
+
+
+
+
+}
